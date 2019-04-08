76b46e30ee1c6f65b684ab76062f6875f7a580e5
Michael Blum <michael.jo.blum@gmail.com>
2019-04-08 16:59:29 -0400

    Hid overflow and incorporated resizability

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild( renderer.domElement );
+
+// update viewport on resize and keep intended/original aspect ratio
+window.addEventListener( 'resize', function(){
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    renderer.setSize(width,height);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+})
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
