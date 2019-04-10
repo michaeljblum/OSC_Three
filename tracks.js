f663b1e4dc690ed620d10a180d0fec3edd820dc5
Michael Blum <michael.jo.blum@gmail.com>
2019-04-10 03:05:48 -0400

    Added big and small models

--- tracks.js
+++ tracks.js
@@ -1,7 +1,12 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-var light = new THREE.AmbientLight(0xffffff, 0.1);
+camera.position.z = 20
+camera.position.y = 5
+camera.position.x = -5
+
+
+var light = new THREE.AmbientLight(0xffffff, 1);
 scene.add(light);
 
 var renderer = new THREE.WebGLRenderer({antialias: true});
@@ -18,9 +23,21 @@ window.addEventListener( 'resize', function(){
     camera.updateProjectionMatrix();
 })
 
+var loader = new THREE.GLTFLoader().setPath( 'models/track/' );
+
+loader.load( 'model.gltf', function ( gltf ) {
+scene.add( gltf.scene );
+} );
+
+var controls = new THREE.OrbitControls( camera, renderer.domElement )
+controls.minDistance = 1
+controls.maxDistance = 1000
+
 function animate() {
     // delta += 0.01;
 	requestAnimationFrame( animate );
     renderer.render( scene, camera );
+
+    controls.update()
 }
 animate();
